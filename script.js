c404398115339f253a7528fb608e8a0dceb7b877
spreston <sam.preston11@gmail.com>
2021-05-14 20:31:23 -0500

    Added arrays for lowercase, uppercase, special character & number storage

--- script.js
+++ script.js
@@ -1,5 +1,119 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = [
+  'a', 
+  'b', 
+  'c', 
+  'd', 
+  'e', 
+  'f', 
+  'g', 
+  'h', 
+  'i', 
+  'j', 
+  'k', 
+  'l', 
+  'm', 
+  'n', 
+  'o', 
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+var upperCase = [
+  'A', 
+  'B', 
+  'C', 
+  'D', 
+  'E', 
+  'F', 
+  'G', 
+  'H', 
+  'I', 
+  'J', 
+  'K', 
+  'L', 
+  'M', 
+  'N', 
+  'O', 
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'X',
+  'X',
+  'Y',
+  'Z'
+];
+
+var specialChar = [
+  '!',
+  '"',
+  '#',
+  '$',
+  '%',
+  '&',
+  "'",
+  '(',
+  ')',
+  '*',
+  '+',
+  ',',
+  '-',
+  '.',
+  '/',
+  ':',
+  ';',
+  '<',
+  '=',
+  '>',
+  '?',
+  '@',
+  '[',
+  ']',
+  '^',
+  '_',
+  '`',
+  '{',
+  '|',
+  '}',
+  '~'
+];
+
+var numberChar = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+];
+
+
+console.log(lowerCase.length);
+console.log(lowerCase);
+console.log(upperCase.length);
+console.log(upperCase);
+console.log(specialChar.length);
+console.log(specialChar);
+console.log(numberChar.length);
+console.log(numberChar);
 
 // Write password to the #password input
 function writePassword() {
